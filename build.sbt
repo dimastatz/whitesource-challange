b570e81470c88492b6c139af87e7096e474a1e91
Dima <dima.statz@gmail.com>
2017-05-05 23:09:40 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,4 @@
+name := "whitesource-challange"
+version := "1.0"
+scalaVersion := "2.12.2"
+        
--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -3,18 +3,28 @@ import scala.io.Source
 /**
   * Created by Dima on 5/5/2017.
   */
-case class Node(value:Int, left: Option[Node], right: Option[Node])
+case class Node(value:Int, var sum: Int)
 
-class Boot {
+object Boot {
   def main(args: Array[String]): Unit = {
-    val root = loadTree(Source.fromResource("triangle.txt").getLines)
-    println(s"max found ${findMax(root)}")
+    val tree = loadTree(Source.fromResource("triangle.txt").getLines)
+    println(s"max found ${tree.last.map(i => i.sum).max}")
   }
 
-  def findMax(node: Node): Int = {
-    if(node.left.isEmpty || node.right.isEmpty) node.value
-    else node.value + Math.max(findMax(node.left.get), findMax(node.right.get))
+  def loadTree(lines: Iterator[String]): Array[Array[Node]] = {
+    val tree = lines.map(i => i.split(" ").map(j => Node(j.toInt, j.toInt))).toArray
+    tree.zipWithIndex.take(tree.length-1).foreach(i => updateWeight(tree, i._2))
+    tree
   }
 
-  def loadTree(lines: Iterator[String]): Node = ???
+  def updateWeight(lines:Array[Array[Node]], index: Int): Unit = {
+    val next = lines(index + 1)
+    lines(index).zipWithIndex.foreach(i =>
+      updateChildren(i._1.sum, next(i._2), next(i._2 + 1)))
+  }
+
+  def updateChildren(value: Int, child1: Node, child2: Node): Unit = {
+    child1.sum = Math.max(child1.sum, child1.value + value)
+    child2.sum = Math.max(child2.sum, child2.value + value)
+  }
 }
